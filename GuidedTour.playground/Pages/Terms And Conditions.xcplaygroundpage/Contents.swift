2b05bd7e34cf1fd43e631de86233f1124375780a
Matthew Harding <matthewpharding@gmail.com>
2023-04-08 14:59:01 +0100

    Replaced the unwanted original licence with terms & conditions.

---
+++ GuidedTour.playground/Pages/Terms And Conditions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+//: ## ![SwiftSimplified.com](swift-simplified-logo.png)
+//: ## The  Swift Guided Tour
+//: ## An Introduction to the Swift Language
+//: This material is an uneditted duplicate of the offical Apple Guided Tour Swift playground, taken from the Swift.org website.
+//:
+//: ![SwiftSimplified.com](ss-in-content-logo.png) [SwiftSimplified.com](https://www.swiftsimplified.com)
+//:
+//: -------------------
+/*:
+## Terms And Conditions
+IMPORTANT:  This software is supplied to you by Swift Simplified. By downloading,
+opening and using this software you agree to these terms and conditions.
+You agree to the following terms. Your use and installation of
+this software constitutes acceptance of these terms.  If you do
+not agree with these terms, please do not use or install this software.
+
+In consideration of your agreement to abide by the following terms, and
+subject to these terms, Swift Simplified grants you a personal, non-exclusive
+license, under Swift Simplifieds copyrights in this original software,
+to use, learn, but not reproduce, modify and redistribute to any other
+persons or platforms with or without modifications, in source and/or
+binary forms;
+
+This software is provided by Swift Simplified on an "AS IS" basis.
+SWIFT SIMPLIFIED OR ANY OTHER AFFILIATES MAKE NO WARRANTIES, EXPRESS OR IMPLIED,
+INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT,
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE SOFTWARE
+OR ITS USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+
+IN NO EVENT SHALL SWIFT SIMPLIFIED OR ANY AFFILIATES BE LIABLE FOR ANY SPECIAL,
+INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES *(INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION)* ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE,
+HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT *(INCLUDING NEGLIGENCE)*,
+STRICT LIABILITY OR OTHERWISE, EVEN IF SWIFT SIMPLIFIED OR ANY AFFILIATES HAS
+BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Copyright (C) 2023 Swift Simplified. All Rights Reserved.
+
+*/
+//:
+//: -------------------
+//:
+//: 👉 [Previous Page](@previous)
+//:
+//: -------------------
+//:
+//: ## ![SwiftSimplified.com](swift-simplified-logo.png)
+//: [Website](https://www.swiftsimplified.com) | [Career Blog](https://www.swiftsimplified.com/swift-career-blog) | [Free Swift Playgrounds](https://www.swiftsimplified.com/swift-playgrounds) | [The  Swift Handbook](https://www.swiftsimplified.com/the-swift-handbook) | [The  Swift Language Guide](https://www.swiftsimplified.com/the-swift-language-guide)
+//:
+//: 🛠 *..let's live a better life, by learning Swift* 🚀
+//:
+//: Welcome to our community of [SwiftSimplified.com](https://www.swiftsimplified.com) students!
+//:
+//: ### 🧕🏻🙋🏽‍♂️👨🏿‍💼👩🏼‍💼👩🏻‍💻💁🏼‍♀️👨🏼‍💼🙋🏻‍♂️🙋🏻‍♀️👩🏼‍💻🙋🏿💁🏽‍♂️🙋🏽‍♀️🙋🏿‍♀️🧕🏾🙋🏼‍♂️
